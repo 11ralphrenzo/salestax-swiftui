3958a3b8d9c19c62f8a67dd3eb6de77b8885c082
Ralph Renzo Pitogo <ralphrenzo96@gmail.com>
2022-11-21 19:18:51 +0800

    Initial Commit

---
+++ SalesTax-SwiftUI/SalesTax_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTax_SwiftUIApp.swift
+//  SalesTax-SwiftUI
+//
+//  Created by Ralph Renzo Pitogo on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SalesTax_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
